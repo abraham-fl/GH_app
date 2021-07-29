91318f978cd286c5ed2a8c920b7829c2ccbcd2d0
Florian <florian.abraham@live.com>
2021-07-30 00:39:40 +0200

    Delete About Page

--- gh_companion.py
+++ gh_companion.py
@@ -1,15 +1,36 @@
-from flask import Flask, escape, request, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "b30e0c86b3e25d58c33d122fac503adf"
 
 @app.route('/')
 @app.route("/home")
 def home():
     return render_template("home.html")
 
-@app.route('/about')
-def about():
-    return f'<h1>About Page</h1>'
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
+    return render_template("register.html", title="Register", form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.username.data == "Flo" and form.password.data == "123":
+            flash("You have been logged in!", "success")
+            return redirect(url_for("home"))
+        else:
+            flash("Login Unsuccessful. Please check username and password.", "danger")
+    return render_template("login.html", title="Login", form=form)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
