1a562434cc7f6bb034cdd7999d1ce89104defbaf
Florian <florian.abraham@live.com>
2021-07-30 18:14:46 +0200

    Add DB functionality and first DB class

--- gh_companion.py
+++ gh_companion.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "b30e0c86b3e25d58c33d122fac503adf"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db" #/// = rel. path
+
+db = SQLAlchemy(app)
+
+class User(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.image_file}')"
+
+
 
 @app.route('/')
 @app.route("/home")
