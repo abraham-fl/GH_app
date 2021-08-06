c4416dcf9c90a3786bab8e7302cc8cd8174fd2ba
Florian Abraham <florian.abraham@fab4minds.com>
2021-08-06 10:44:58 +0200

    Add login capabilities

--- gh_companion/__init__.py
+++ gh_companion/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -7,5 +9,9 @@ app.config["SECRET_KEY"] = "b30e0c86b3e25d58c33d122fac503adf"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db" #/// = rel. path
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login" #route die aufgerufen wird, wenn ein nicht eingeloggter User versucht, eine nicht erlaubte Seite zu öffnen
+login_manager.login_message_category = "info" #"info" == bootstrap info class
 
 from gh_companion import routes
--- gh_companion/routes.py
+++ gh_companion/routes.py
@@ -1,7 +1,9 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from gh_companion import app, db, bcrypt
 from gh_companion.forms import RegistrationForm, LoginForm
 from gh_companion.models import User, Character, Perk
-from gh_companion import app
+from flask_login import login_user, current_user, logout_user, login_required
+
 
 @app.route('/')
 @app.route("/home")
@@ -10,19 +12,39 @@ def home():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f"Account created for {form.username.data}!", "success")
-        return redirect(url_for("home"))
+        hashed_pw = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+        user = User(username = form.username.data, password = hashed_pw)
+        db.session.add(user)
+        db.session.commit()
+        flash(f"Your account has been created! You are now able to login.", "success")
+        return redirect(url_for("login"))
     return render_template("register.html", title="Register", form=form)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.username.data == "Flo" and form.password.data == "123":
-            flash("You have been logged in!", "success")
-            return redirect(url_for("home"))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get("next") # access URL parameters
+            return redirect(next_page) if next_page else redirect(url_for("home"))
         else:
             flash("Login Unsuccessful. Please check username and password.", "danger")
     return render_template("login.html", title="Login", form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template("account.html", title="Account")
