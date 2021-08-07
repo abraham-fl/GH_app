a8a6b0b73f2fad33dc9f7ad50686e3b0ce64b1de
Florian <florian.abraham@live.com>
2021-08-07 13:10:06 +0200

    Fix variable assignment in config file

--- gh_companion/config.py
+++ gh_companion/config.py
@@ -1,3 +1,3 @@
 class Config:
-    "SECRET_KEY" = "b30e0c86b3e25d58c33d122fac503adf"
-    "SQLALCHEMY_DATABASE_URI" = "sqlite:///site.db" #/// = rel. path
+    SECRET_KEY = "b30e0c86b3e25d58c33d122fac503adf"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///site.db" #/// = rel. path
